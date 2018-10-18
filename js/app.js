ef38e82cefbbc23b4c88cd7f9072f041c307adf8
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-18 08:09:08 -0400

    Create method, propeties and form to create new Tasks

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
-new Vue({
+const app = new Vue({
     el: "#app",
     data: {
-        message:"Welcome to Vue"
+        title: 'Todo List',
+        tasks : [ 
+            {
+                text : "Learn Vue Js",
+                finished: false
+            },
+            {
+                text : "Build App",
+                finished: false
+            },
+            {
+                text : "Build Todo List",
+                finished: false
+            }
+        ],
+        newTask: ''
+    },
+    methods: {
+        addTask:function(){
+            let text = this.newTask.trim();
+            if(text) {
+                this.tasks.push({
+                    text:text,
+                    finished:false
+                });
+            }
+            this.newTask = "";
+        }
     }
 })
